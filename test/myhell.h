a6f868fdcead726f9a68995764eaa9305ce51081
MyAtlas0 <AbdullahiOlawale09@gmail.com>
2023-12-12 20:19:01 -0800

    test fix shell environment

--- test/myhell.h
+++ test/myhell.h
@@ -12,37 +12,58 @@
 #define MAX_INPUT 1024
 #define INITIAL_BUFFER_SIZE 128
 
-/**  Prototype Functions, It's declarations  **/
-int main();
-int _atoi(char *s);
+/**  Prompt Prototype, And it's Helper Functions  **/
 
+int main(void);
+ssize_t write_char(const char *input);
+char *read_prompt();
+void free_args(char **args);
+void input_process(char *input);
+
+
+/** Custom_GetLine Function, It's declarations **/
+
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+void expand_buffer(char **lineptr, size_t *n);
+
+
+
+/** Custom_Strtok Function, It's declaration **/
+
+char **_strtok(const char *input);
+
+
+
+/** Custom_String Functions, Their declarations **/
+
+char *_strndup(const char *first, size_t n);
+
+
+
+/** Commands Prototype Functions, It's declarations **/
+
+char **process_arguments(const char *args[], const char *commands_args);
+void execute_command(const char *path, const char *args[]);
+void execute_ls(const char *args[]);
+void execute_pwd(void);
+
+
+
+/** Environment Functions, It's declarations **/
+
+extern char **environ;
+void print_environment(void);
 char *custom_getenv(const char *name, char **envp);
 int custom_setenv(const char *name, const char *value, int overwrite);
 int custom_unsetenv(const char *name);
 
-char *read_prompt();
-ssize_t write_char(const char *input);
-void free_args(char **args);
-void input_process(char *input);
+
+
+/** Handle_Commands Prototype, And it's Helper Functions. **/
+
+int _atoi(char *s);
 void handle_commands(char **args, char *input);
 void handle_env_command(char **args);
-void execute_other_commands(char **args);
-void execute_command(const char *path, const char *args[]);
-void execute_ls(const char *args[]);
-void execute_pwd(void);
-void print_environment(void);
-extern char **environ;
-void clear_environment();
-
-/** Custome _Strtok protopype, and helper Function **/
-char **_strtok(const char *input);
-char *_strndup(const char *first, size_t n);
-
-
-char **process_arguments(const char *args[], const char *commands_args);
-
-
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
-void expand_buffer(char **lineptr, size_t *n);
+void handle_other_commands(char **args);
 
 #endif /**  MYHELL_H  **/
